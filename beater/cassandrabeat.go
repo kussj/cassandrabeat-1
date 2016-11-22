bdf539a8dcbdb879d0cbf3b96e95d1bc824e6a7d
Justin Kuss <justin.kuss@gmail.com>
2016-11-22 10:55:56 -0500

    Adding new metrics collection into cassandrabeat.

--- beater/cassandrabeat.go
+++ beater/cassandrabeat.go
@@ -16,11 +16,11 @@ import (
 )
 
 type Cassandrabeat struct {
-	done       chan struct{}
-	config     config.Config
-	client     publisher.Client
+	done   chan struct{}
+	config config.Config
+	client publisher.Client
 
-	table      []string
+	table []string
 }
 
 // Creates beater
@@ -31,7 +31,7 @@ func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
 	}
 
 	bt := &Cassandrabeat{
-		done: make(chan struct{}),
+		done:   make(chan struct{}),
 		config: config,
 	}
 	return bt, nil
@@ -52,7 +52,7 @@ func (bt *Cassandrabeat) Run(b *beat.Beat) error {
 
 		for _, table := range bt.table {
 			logp.Info("Getting latency for table: %s", table)
-			bt.getLatency(table)
+			bt.getMetrics(table)
 		}
 		logp.Info("Event sent")
 	}
@@ -63,33 +63,90 @@ func (bt *Cassandrabeat) Stop() {
 	close(bt.done)
 }
 
-func (bt *Cassandrabeat) getLatency(table string) {
+func (bt *Cassandrabeat) getMetrics(table string) {
 	cmdName := "awkscript.sh"
 	cmdArgs := []string{table}
 	cmdOut := exec.Command(cmdName, cmdArgs...).Output
 
-	output, _ := cmdOut()
-	latency := strings.Split(string(output), "\n")
-
-	var read_latency, write_latency float64
-	if strings.Compare(latency[0], "NaN") == 0 {
-		read_latency = 0.0
-	} else {
-		read_latency, _ = strconv.ParseFloat(latency[0], 64)
-	}
-	if strings.Compare(latency[1], "NaN") == 0 {
-		write_latency = 0.0
-	} else {
-		write_latency, _ = strconv.ParseFloat(latency[1], 64)
+	output, err := cmdOut()
+	if err != nil {
+		fmt.Println(err)
+		time.Sleep(5000 * time.Millisecond)
+		return
 	}
 
-	event := common.MapStr {
-		"@timestamp":	 common.Time(time.Now()),
-		"type":		 "stats",
-		"count":	 1,
-		"table_name":	 table,
-		"write_latency": write_latency,
-		"read_latency":	 read_latency,
+	metrics := strings.Split(string(output), "\n")
+
+	/*
+		fmt.Printf("Results back from nodetool cfstats for %s\n", table)
+		for i := range latency {
+			fmt.Println(i, latency[i])
+		}
+	*/
+
+	if len(metrics) < 8 {
+		fmt.Printf("Not enough values (%v) returned from nodetool script. Bailing.\n", len(metrics))
+		return
+	}
+
+	var readLatency, writeLatency float64
+	var pendingFlushes, sstableCount, spaceUsedLive, spaceUsedTotal int64
+	var spaceUsedSnapshotTotal, numberOfKeys int64
+
+	if strings.Compare(metrics[0], "NAN") == 0 {
+		pendingFlushes = 0
+	} else {
+		pendingFlushes, _ = strconv.ParseInt(metrics[0], 10, 64)
+	}
+	if strings.Compare(metrics[1], "NAN") == 0 {
+		sstableCount, _ = strconv.ParseInt(metrics[1], 10, 64)
+	} else {
+		sstableCount = 0
+	}
+	if strings.Compare(metrics[2], "NAN") == 0 {
+		spaceUsedLive, _ = strconv.ParseInt(metrics[2], 10, 64)
+	} else {
+		spaceUsedLive = 0
+	}
+	if strings.Compare(metrics[3], "NAN") == 0 {
+		spaceUsedTotal, _ = strconv.ParseInt(metrics[3], 10, 64)
+	} else {
+		spaceUsedTotal = 0
+	}
+	if strings.Compare(metrics[4], "NAN") == 0 {
+		spaceUsedSnapshotTotal, _ = strconv.ParseInt(metrics[4], 10, 64)
+	} else {
+		spaceUsedSnapshotTotal = 0
+	}
+	if strings.Compare(metrics[5], "NAN") == 0 {
+		numberOfKeys, _ = strconv.ParseInt(metrics[5], 10, 64)
+	} else {
+		numberOfKeys = 0
+	}
+	if strings.Compare(metrics[6], "NaN") == 0 {
+		readLatency = 0.0
+	} else {
+		readLatency, _ = strconv.ParseFloat(metrics[6], 64)
+	}
+	if strings.Compare(metrics[7], "NaN") == 0 {
+		writeLatency = 0.0
+	} else {
+		writeLatency, _ = strconv.ParseFloat(metrics[7], 64)
+	}
+
+	event := common.MapStr{
+		"@timestamp":                common.Time(time.Now()),
+		"type":                      "stats",
+		"count":                     1,
+		"table_name":                table,
+		"write_latency":             writeLatency,
+		"read_latency":              readLatency,
+		"pending_flushes":           pendingFlushes,
+		"sstable_count":             sstableCount,
+		"space_used_live":           spaceUsedLive,
+		"space_used_total":          spaceUsedTotal,
+		"space_used_snapshot_total": spaceUsedSnapshotTotal,
+		"number_of_keys":            numberOfKeys,
 	}
 
 	bt.client.PublishEvent(event)
